b02a5a659dff5bb6a2ea1edbff641fc86134d6c6
Zeeshan Ahmad Khalil <41861952+ZeeshanAhmadKhalil@users.noreply.github.com>
2023-10-10 01:40:05 +0500

    - setup the nest js project - added swagger - internally calling the github repo issues API

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IssueModule } from '@modules/issue/issue.module';
 
 @Module({
-  imports: [],
+  imports: [
+    IssueModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
